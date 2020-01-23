7cff8e71d2ae3918fa10b8e9e663494bc495afa1
Charles Hong <charleshong@berkeley.edu>
2020-01-23 00:08:48 -0800

    deleted copy_git.sh from starter code

--- lab00/copy_git.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-rm -rf ./git
-rm -f git.tar
-tar -C .. -cf ./git.tar .git
-git add ./git.tar
-git commit -m "Added the git repo metadata tar to your lab00 folder"
-git push origin master
